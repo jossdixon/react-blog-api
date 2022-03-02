ea67135d70d4d875e6d82f4960e7216def935e6c
Joss Dixon <josscdixon@gmail.com>
2022-03-02 09:50:13 +0000

    updated controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,67 +1,36 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
-  end
-
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
-  end
-
-  # GET /posts/1/edit
-  def edit
+    @posts = Post.order("created_at DESC")
+    render json: posts
   end
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.create(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: post
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post = Post.find([params[:id]])
+    @post.update(post_params)
+    render json: post
   end
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    head :no_content, status: :ok
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+    # def set_post
+    #   @post = Post.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def post_params
