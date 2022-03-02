8c75fa6c46742aad9aa3f3aa9c69136a5a097bdd
Joss Dixon <josscdixon@gmail.com>
2022-03-02 10:44:29 +0000

    modified application rb

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Rails.application.config.hosts << "https://dixonreactblog.netlify.app/"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,6 +11,7 @@ module ReactBlogApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Rails.application.config.hosts << "https://dixonreactblog.netlify.app/"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
