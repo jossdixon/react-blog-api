8ffdae013c3b2e718a0d24f3d367148d30071970
Joss Dixon <josscdixon@gmail.com>
2022-03-02 10:42:02 +0000

    updated application rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+Rails.application.config.hosts << "https://dixonreactblog.netlify.app/"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
